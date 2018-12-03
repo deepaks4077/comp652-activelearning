e56f3cbb0999f71245f1d6a9f43e5654b3c9b106
Deepak Sharnma <deepaks4077@gmail.com>
2018-12-02 22:07:31 -0500

    Added Selector class

---
+++ Selector.py
@@ -0,0 +1,12 @@
+from typing import Tuple, List, Union, Dict
+import torch
+import torch.nn as nn
+import numpy as np
+from AcquisitionFunctions import AcquisitionFunction
+
+class Selector():
+    def __init__(self, func: AcquisitionFunction):
+        self.func = func
+
+    def select(self, input_ids, outputs):
+        return self.func.get_best_sample(input_ids, outputs)
